d7ba237e8547cc3cddec1c533b19b9a4c0ca8d7f
Jaime Leon <jleonhol@gmail.com>
2019-09-10 12:02:16 +0200

    Added print.sh for simplicity

---
+++ test/print.sh
@@ -0,0 +1,5 @@
+mkdir /eos/home-j/jleonhol/www/run330848_2/
+mkdir /eos/home-j/jleonhol/www/run330848_2/plots
+rm /eos/home-j/jleonhol/www/run330848_2/plots/*
+cp ./run330848/* /eos/home-j/jleonhol/www/run330848_2/plots/
+cp results_run330848.root /eos/home-j/jleonhol/www/run330848_2/
