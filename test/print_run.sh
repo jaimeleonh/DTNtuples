169164639983af343dad7c0dfcef56a753923811
Jaime Leon <jleonhol@gmail.com>
2019-09-19 10:55:18 +0200

    Change folders

--- test/print_run.sh
+++ test/print_run.sh
@@ -1,7 +1,7 @@
-mkdir /eos/home-j/jleonhol/www/run$1/
-mkdir /eos/home-j/jleonhol/www/run$1/plots
-rm /eos/home-j/jleonhol/www/run$1/plots/*
-cp ./run$1/* /eos/home-j/jleonhol/www/run$1/plots/
-cp results_run$1.root /eos/home-j/jleonhol/www/run$1/
+mkdir /eos/home-j/jleonhol/www/SliceTest/run$1/
+mkdir /eos/home-j/jleonhol/www/SliceTest/run$1/plots
+rm /eos/home-j/jleonhol/www/SliceTest/run$1/plots/*
+cp ./run$1/* /eos/home-j/jleonhol/www/SliceTest/run$1/plots/
+cp results_run$1.root /eos/home-j/jleonhol/www/SliceTest/run$1/
 rm -rf ./run$1
-cp /eos/home-j/jleonhol/www/index_php /eos/home-j/jleonhol/www/run$1/index.php 
+cp /eos/home-j/jleonhol/backup/index_php /eos/home-j/jleonhol/www/SliceTest/run$1/index.php 
