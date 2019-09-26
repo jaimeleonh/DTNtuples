eafbba14583d9b4a086eebb9a4a121f46d25101b
Jaime Leon <jleonhol@gmail.com>
2019-09-26 10:19:53 +0200

    Stuff

---
+++ test/copyToEos.sh
@@ -0,0 +1,7 @@
+rm /eos/home-j/jleonhol/www/resolutions*/*
+cp /eos/home-j/jleonhol/backup/index_resol_php /eos/home-j/jleonhol/www/resolutionsAM/index.php
+cp /eos/home-j/jleonhol/backup/index_resol_php /eos/home-j/jleonhol/www/resolutionsAMST/index.php
+cp /eos/home-j/jleonhol/backup/index_resol_php /eos/home-j/jleonhol/www/resolutionsHB/index.php
+cp ./plotsPU0ST/*AM*.png /eos/home-j/jleonhol/www/resolutionsAMST 
+cp ./plotsPU0/*AM*.png /eos/home-j/jleonhol/www/resolutionsAM 
+cp ./plotsPU0/*HB*.png /eos/home-j/jleonhol/www/resolutionsHB 
