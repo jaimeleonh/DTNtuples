f56841712804120a6892fee5adedef8f0d6ef94c
Jaime Leon <jleonhol@gmail.com>
2019-09-30 15:30:19 +0200

    Adding stuff

--- test/copyToEos.sh
+++ test/copyToEos.sh
@@ -1,7 +1,12 @@
-rm /eos/home-j/jleonhol/www/resolutions*/*
+mkdir /eos/home-j/jleonhol/www/resolutionsAM
+mkdir /eos/home-j/jleonhol/www/resolutionsHB
+mkdir /eos/home-j/jleonhol/www/resolutionsAMAllST
+rm /eos/home-j/jleonhol/www/resolutionsAM/*
+rm /eos/home-j/jleonhol/www/resolutionsHB/*
+rm /eos/home-j/jleonhol/www/resolutionsAMST/*
 cp /eos/home-j/jleonhol/backup/index_resol_php /eos/home-j/jleonhol/www/resolutionsAM/index.php
 cp /eos/home-j/jleonhol/backup/index_resol_php /eos/home-j/jleonhol/www/resolutionsAMST/index.php
 cp /eos/home-j/jleonhol/backup/index_resol_php /eos/home-j/jleonhol/www/resolutionsHB/index.php
 cp ./plotsPU0ST/*AM*.png /eos/home-j/jleonhol/www/resolutionsAMST 
 cp ./plotsPU0/*AM*.png /eos/home-j/jleonhol/www/resolutionsAM 
-cp ./plotsPU0/*HB*.png /eos/home-j/jleonhol/www/resolutionsHB 
+cp ./plotsPU0/*HB*.png /eos/home-j/jleonhol/www/resolutionsHB
