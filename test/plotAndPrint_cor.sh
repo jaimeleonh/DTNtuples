aafd7f9020e546e89326cde0404d66fbf2108947
Jaime Leon <jleonhol@gmail.com>
2019-10-23 16:12:04 +0200

    plotting tools

---
+++ test/plotAndPrint_cor.sh
@@ -0,0 +1,10 @@
+rm ./run$1/*
+root -b << EOF
+.x loadTPGSimAnalysis.C
+DTNtupleTPGSimAnalyzer analysis("/afs/cern.ch/work/j/jleonhol/public/DTDPGNtuple_run$1_corrected.root","results_run$1.root",false)
+analysis.Loop()
+EOF
+
+root -b << EOF 
+.x printPlots_run.C ("$1")
+EOF
