3c8f5a98ad1bc0f80974f42c071d789c404ef243
Jaime Leon <jleonhol@gmail.com>
2020-01-21 09:33:18 +0100

    Removing subfolders

--- test/print_run.sh
+++ test/print_run.sh
@@ -4,7 +4,6 @@ echo "INFO: Deleted old files"
 #cp -r ./run$1/* /eos/home-j/jleonhol/www/SliceTest/run$1/
 cp results_run$1.root /eos/home-j/jleonhol/www/SliceTest/run$1/
 echo "INFO: Ntuple copied"
-#rm -rf ./run$1
 cp /eos/home-j/jleonhol/backup/index_withFolders_php /eos/home-j/jleonhol/www/SliceTest/run$1/index.php 
 for i in ./run$1/*
 do
@@ -16,4 +15,5 @@ do
     cp /eos/home-j/jleonhol/backup/index_noFolders_php /eos/home-j/jleonhol/www/SliceTest/$i/index.php 
   fi
 done
+rm -rf ./run$1
 
