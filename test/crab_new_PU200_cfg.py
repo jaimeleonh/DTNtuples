d013983e1a92d2475ed46545b3c54824b12b9ce5
Jaime Leon <jleonhol@gmail.com>
2020-02-25 15:29:35 +0100

    Added some samples to crab submit file

--- test/crab_new_PU200_cfg.py
+++ test/crab_new_PU200_cfg.py
@@ -3,7 +3,12 @@ config = Configuration()
 
 ##### Configuration parameters ################################
 
-inputDataset = "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW"
+#inputDataset = "/DisplacedMuons_Pt10to30_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW"
+#inputDataset = "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW"
+#inputDataset = "/Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW"
+inputDataset = "/DisplacedMuons_Pt10to30_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW"
+#inputDataset = "/HTo2LongLivedTo4mu_MH-200_MFF-50_CTau-2000mm_TuneCP5_13TeV_pythia8/escalant-MC2018_Benchmark_2mu2jets_June2019-GSR-testForL1-v2-5aa1749307f00d6302ec929df355f761/USER"
+#inputDataset = "/DisplacedMuons_Pt30to100_Dxy0to3000-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW"
 
 # These are the cfg parameters used to configure the 
 # dtDpgNtuples_slicetest_cfg.py configuration file
@@ -22,7 +27,10 @@ inputFiles = []
 
 config.section_('General')
 config.General.workArea = 'crab_jobs'
-config.General.requestName = 'DTDPGNtuples_PU200_corFilter'
+#config.General.requestName = 'DTDPGNtuples_PU200_1.5'
+#config.General.requestName = 'DTDPGNtuples_PU200_newCut'
+config.General.requestName = 'DTDPGNtuples_DM_PU0_corSeg_new'
+#config.General.requestName = 'DTDPGNtuples_PU0_DM_PT10-30_mod'
 config.General.transferOutputs = True
 
 config.section_('JobType')
@@ -37,9 +45,12 @@ config.Data.inputDataset = inputDataset
 
 config.Data.splitting    = 'LumiBased'
 config.Data.unitsPerJob  = 10  
+#config.Data.inputDBS = 'phys03'
 #config.Data.inputDBS     = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
-config.Data.outLFNDirBase  = '/store/user/jleonhol/PU200_corFilter'
+#config.Data.outLFNDirBase  = '/store/user/jleonhol/PU200_newCut'
+config.Data.outLFNDirBase  = '/store/user/jleonhol/PU0_DM_PT10-30_corSeg_new'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_ES_CIEMAT'
+#config.Site.whitelist = ['T2_PL_Warsaw']
 
